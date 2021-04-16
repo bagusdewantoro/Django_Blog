3edb0430d8f5c4560d66bf0e5f0448f095aba04e
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-04-16 11:37:38 +0700

    separated settings

--- Django_Blog/settings - Production.py
+++ Django_Blog/settings - Production.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'a45c0c7100350119a146c11aae69181db581384a2570faf0')  
+SECRET_KEY = os.environ.get('SECRET_KEY')  
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False   
 
-ALLOWED_HOSTS = ['bagusblog.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['bagusblog.herokuapp.com']
 
 
 # Application definition
